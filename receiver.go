29b66fb4bae55c69a8788671ab08f702834d7bac
Andrey Arkaev <aarkaev@naumen.ru>
2017-12-19 10:59:09 +0500

    use byte array for commands instead of string

--- receiver.go
+++ receiver.go
@@ -10,48 +10,48 @@ import (
 
 //Receiver for commands from socket
 func startReceiver(nc *Conn) {
-	socketToStrCommand := make(chan string)
-	strToNode := make(chan *Node)
+	socketToDataCommand := make(chan []byte)
+	dataToNode := make(chan *Node)
 	nodeToHanlderChannel := make(chan Handler)
 
 	go func() {
 		bufReader := bufio.NewReader(nc.conn)
 		for {
-			msg, err := bufReader.ReadString(delimeter)
+			cmdData, err := bufReader.ReadBytes(delimeter)
 			if err != nil {
 				if err == io.EOF {
 					//sleep if no data
 					time.Sleep(time.Millisecond * 10)
 				} else {
-					log.Println("Unexpected read error: ", err)
+					log.Printf("Unexpected read error: %s\n", err)
 					break
 				}
 			} else {
-				log.Println("Received:\n" + msg)
-				socketToStrCommand <- msg
+				log.Printf("Received:%s\n", cmdData)
+				socketToDataCommand <- cmdData
 			}
 		}
 	}()
 
 	go func() {
 		for {
-			cmdStr := <-socketToStrCommand
+			cmdData := <-socketToDataCommand
 
 			root := Node{}
-			err := xml.Unmarshal([]byte(cmdStr), &root)
+			err := xml.Unmarshal(cmdData, &root)
 			if err != nil {
 				log.Printf("error parsing command: %v\n", err)
 			}
 
 			for _, event := range root.Nodes {
-				strToNode <- event
+				dataToNode <- event
 			}
 		}
 	}()
 
 	go func() {
 		for {
-			event := <-strToNode
+			event := <-dataToNode
 			rootTag := event.XMLName.Local
 
 			handler, exist := nc.handlers[rootTag]
